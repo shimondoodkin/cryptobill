329053e494d98305d52df8df2ac0edcdfe4bf6c4
Shimon Doodkin <helpmepro1@gmail.com>
2017-10-22 12:03:37 +0300

    Update console_font_6x8.js

--- console_font_6x8.js
+++ console_font_6x8.js
@@ -1,5 +1,18 @@
 // font from https://github.com/idispatch/raster-fonts
 
+function chunk (arr, len) {
+
+  var chunks = [],
+      i = 0,
+      n = arr.length;
+
+  while (i < n) {
+    chunks.push(arr.slice(i, i += len));
+  }
+
+  return chunks;
+}
+
 var console_font_6x8 = chunk([
 
     /*
